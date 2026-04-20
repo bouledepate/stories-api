5119e612b33e954668922fc8a4de070214f3dca4
bouledepate <80426332+bouledepate@users.noreply.github.com>
2026-04-20 11:07:11 +0500

    Add middleware layer with request logging and Yii validation

--- config/container.php
+++ config/container.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 use DI\ContainerBuilder;
 use Doctrine\DBAL\Connection;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Slim\Psr7\Factory\ResponseFactory;
 use Stories\Shared\Database\ConnectionFactory;
 use Stories\Shared\Security\JwtService;
 use Stories\Slices\Rooms\Service\RoomService;
+use Yiisoft\Validator\Validator;
 
 return static function (array $env): ContainerBuilder {
     $dbPath = (string) ($env['DB_PATH'] ?? (__DIR__ . '/../var/data.sqlite'));
@@ -22,6 +25,8 @@ return static function (array $env): ContainerBuilder {
     $builder->addDefinitions([
         Connection::class => static fn (): Connection => ConnectionFactory::create($dbPath),
         JwtService::class => static fn (): JwtService => new JwtService($jwtSecret),
+        Validator::class => static fn (): Validator => new Validator(),
+        ResponseFactoryInterface::class => static fn (): ResponseFactoryInterface => new ResponseFactory(),
         RoomService::class => DI\autowire()->constructorParameter('disconnectGraceSeconds', $disconnectGraceSeconds),
     ]);
 
